f3e54f5ae89c7defa9ff86b73b64de9443431710
DELL <omnianabil1920@gmail.com>
2024-07-12 17:23:37 +0300

    displaying items

--- app/src/main/java/com/example/thesportsapp/CustomAdapter.java
+++ app/src/main/java/com/example/thesportsapp/CustomAdapter.java
@@ -3,13 +3,15 @@ package com.example.thesportsapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class CustomAdapter extends RecyclerView.Adapter<> {
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.SportsViewHolder> {
 
     private List<Sport> sportList;
 
@@ -19,25 +21,33 @@ public class CustomAdapter extends RecyclerView.Adapter<> {
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public SportsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item_layout,parent,false);
         return new SportsViewHolder(itemView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull SportsViewHolder holder, int position) {
+        Sport sport = sportList.get(position);
+        holder.imageView.setImageResource(sport.image);
+        holder.textView.setText(sport.titleText);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return sportList.size();
     }
 
     public static class SportsViewHolder extends RecyclerView.ViewHolder {
+      ImageView imageView;
+      TextView textView;
 
         public SportsViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            imageView = itemView.findViewById(R.id.imageView);
+            textView = itemView.findViewById(R.id.titleTextView);
         }
     }
 }
--- app/src/main/java/com/example/thesportsapp/MainActivity.java
+++ app/src/main/java/com/example/thesportsapp/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.thesportsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private List<Sport> sportList;
+    CustomAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView =findViewById(R.id.recyclerView);
+        sportList = new ArrayList<>();
+        Sport s1 = new Sport("Football", R.drawable.football);
+        Sport s2 = new Sport("Basketball", R.drawable.basketball);
+        Sport s3 = new Sport("VolleyBall", R.drawable.volley);
+        Sport s4 = new Sport("Tennis", R.drawable.tennis);
+        Sport s5 = new Sport("Ping Pong", R.drawable.ping);
+
+        sportList.add(s1);
+        sportList.add(s2);
+        sportList.add(s3);
+        sportList.add(s4);
+        sportList.add(s5);
+
+         adapter = new CustomAdapter(sportList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
     }
 }
